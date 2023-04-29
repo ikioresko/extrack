edec4c64b6595f4a697a689dfcb8b77ff0eb46ea
ikioresko <ikioresko@gmail.com>
2023-04-29 12:26:34 +0500

    RegexService

---
+++ src/main/java/com/home/extrack/service/RegexService.java
@@ -0,0 +1,41 @@
+package com.home.extrack.service;
+
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+
+@Service
+public class RegexService {
+    private final RegexMemPool regexMemPool;
+
+    public RegexService(RegexMemPool regexMemPool) {
+        this.regexMemPool = regexMemPool;
+    }
+
+    /**
+     * Looking for regex by input string
+
+     * @param errorItem errorString
+     */
+    public Long regexSearch(String errorItem) {
+        String error = errorItem.replaceAll(System.lineSeparator(), "\n");
+        List<Long> regexId = new ArrayList<>();
+        ConcurrentHashMap<Long, Pattern> patternMap = regexMemPool.getMap();
+        Matcher matcher;
+        for (Map.Entry<Long, Pattern> entry : patternMap.entrySet()) {
+            matcher = entry.getValue().matcher(error);
+            if (matcher.find()) {
+                regexId.add(entry.getKey());
+                break;
+            }
+            matcher.reset();
+        }
+        return regexId.size() >= 1 ? regexId.get(0) : null;
+    }
+}
