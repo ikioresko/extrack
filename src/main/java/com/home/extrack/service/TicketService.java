70f699f0a06e88cea2ffe41991a83a5bf8499fe4
ikioresko <ikioresko@gmail.com>
2023-05-04 08:55:35 +0500

    TicketService fix

--- src/main/java/com/home/extrack/service/TicketService.java
+++ src/main/java/com/home/extrack/service/TicketService.java
@@ -11,8 +11,10 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
+import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
+@Service
 public class TicketService {
     private final RestTemplate restTemplate;
     @Value("${serviceHeader}")
